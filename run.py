26fb56fd44866d5d4f347528538e6e6a738e8e34
cwb-05 bass <Coyotekid@t-online.de>
2024-08-11 15:42:21 +0000

    Ass validation to check for exactly 6 values in provided data

--- run.py
+++ run.py
@@ -17,11 +17,29 @@ def get_sales_data():
     """
     Get sales figures input from the user
     """
-print("Please enter sales data from the last market.")
-print("Data should be six numbers, separated by commas.")
-print("Example: 10,20,30,40,50,60\n")
+    print("Please enter sales data from the last market.")
+    print("Data should be six numbers, separated by commas.")
+    print("Example: 10,20,30,40,50,60\n")
 
-data_str = input("Enter your data here: ")
-print(f"The data provided is {data_str}")
+    data_str = input("Enter your data here: ")
+
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+
+
+def validate_data(values):
+    """
+    Inside the try, converts all string values to integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there arent exactly 6 values.
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f"eyactly 6 values required, you provided {len(values)}"
+            ) 
+    except ValueError as e:
+        print (f"Invalid data: {e}, please tr< again.\n")
+
 
 get_sales_data()
